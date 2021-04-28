fb096faff383df4341c7e15673875fc45d611ab9
Aditya Chanana <adityachanana123@gmail.com>
2021-04-28 02:08:40 -0400

    Add eslint and prettier to npm test

--- frontend/src/contexts/LoctasticContext.ts
+++ frontend/src/contexts/LoctasticContext.ts
@@ -1,7 +1,7 @@
-import { createContext } from 'react';
-import { menuItem } from '../App'
+import { createContext } from "react";
+import { menuItem } from "../App";
 
-const defaultMenuItems : menuItem[] = [];
+const defaultMenuItems: menuItem[] = [];
 export const LoctasticContext = createContext({ menuItems: defaultMenuItems });
 const LoctasticContextProvider = LoctasticContext.Provider;
 export default LoctasticContextProvider;
