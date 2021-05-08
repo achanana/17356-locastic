1d4227142cde0c99cf15cd54b96de2cdc58632c5
zainimran <zainimran@outlook.com>
2021-05-08 15:39:58 -0400

    fix basic app jest test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/loctastic/i);
+  const linkElement = screen.getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
